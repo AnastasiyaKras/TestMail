52e3409ba8a40e8e4d3ae894f9b4428af3d9b1e0
AnastasiyaKras <nikkikillerz@mail.ru>
2019-03-07 17:31:07 +0400

    Create PageObjectBase.cs

---
+++ UnitTest/PageObject/PageObjectBase.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Configuration;
+
+namespace UnitTest.PageObject
+{
+    public class PageObjectBase<T> where T : PageObjectBase<T>
+    {
+        protected IWebDriver driver;
+        protected WebDriverWait wait;
+
+    public virtual string ShortUrl
+        {
+            get
+            {
+                return "";
+            }
+        }
+    public string Url
+        {
+            get
+            {
+                return ShortUrl.Contains("http") || ShortUrl.Contains("www")
+                    ?ShortUrl
+                    : ConfigurationManager.AppSettings.Get("BaseUrl") + ShortUrl;
+            }
+        }
+    public T Open()
+        {
+            // открываем страницу почты
+            driver.Navigate().GoToUrl(Url);
+            return (T)this;
+        }
+    public PageObjectBase(IWebDriver driver)
+        {
+            this.driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2000));
+        }
+    }
+}
